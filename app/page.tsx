6f3ab061a1eec054e55d5155163b772f1762cec2
chekirko <victorche59@gmail.com>
2024-10-30 22:05:53 +0200

    setup shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <h1 className="text-3xl font-bold text-violet-700 underline">Home</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-violet-700 underline">Home</h1>
+    </>
+  );
 };
 
 export default Home;
