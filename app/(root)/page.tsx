0e557fc7c0376a2162a17cc93efe2cb0512580f5
chekirko <victorche59@gmail.com>
2024-11-01 21:32:15 +0200

    implement Local Search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,51 @@ import { auth, signOut } from "@/auth";
 
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import LocalSearch from "@/components/search/LocalSearch";
 
-const Home = async () => {
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "How to learn JavaScript?",
+    description: "I want to learn JavaScript, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+const Home = async ({ searchParams }: SearchParams) => {
   const session = await auth();
+
+  const { query = "" } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLowerCase().includes(query?.toLowerCase())
+  );
+
   return (
     <>
       <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -18,24 +60,34 @@ const Home = async () => {
           <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
         </Button>
       </section>
-      <section className="mt-11">LocalSearch</section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
       HomeFilter
       <div className="mt-10 flex w-full flex-col gap-6">
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
-        <p>Question Card 1</p>
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
       </div>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
+      {session ? (
+        <form
+          className="px-10 pt-[100px]"
+          action={async () => {
+            "use server";
 
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
+            await signOut({ redirectTo: ROUTES.SIGN_IN });
+          }}
+        >
+          <Button type="submit">Log out</Button>
+        </form>
+      ) : (
+        <></>
+      )}{" "}
     </>
   );
 };
